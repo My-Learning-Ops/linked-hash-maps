4cf1b24846141b0c469e9bc48cd2896080e1c10c
brendan-dileo <brendan.dileo@marmonrail.com>
2025-07-17 16:17:10 -0400

    Update main to test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,18 @@
-package org.example;
 
 public class Main {
 
     public static void main(String[] args) {
 
         LinkedHashMap<String, Integer> linkedHashMap = new LinkedHashMap<>();
+
+        try {
+            linkedHashMap.put("One", 1);
+            linkedHashMap.put("Two", 2);
+            linkedHashMap.put("Three", 3);
+            linkedHashMap.put("One", 11);
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
 
     }
 
