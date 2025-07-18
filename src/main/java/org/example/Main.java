014ab3e84a3724ad281193e29c618e97d4cae88d
Brendan Dileo <dileob23@gmail.com>
2025-07-18 09:34:54 -0400

    Add tests to main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,10 @@ public class Main {
             linkedHashMap.put("One", 1);
             linkedHashMap.put("Two", 2);
             linkedHashMap.put("Three", 3);
+            linkedHashMap.put("Four", 4);
+            linkedHashMap.put("Five", 5);
+            linkedHashMap.put("Six", 6);
+            linkedHashMap.put("Seven", 7);
 
             // Update existing keys value
             linkedHashMap.put("One", 11);
@@ -17,10 +21,21 @@ public class Main {
             // Test retrieval
             System.out.println("\nGet 'Two': " + linkedHashMap.get("Two"));
             System.out.println("Get 'Four': " + linkedHashMap.get("Four"));
+            System.out.println("Get 'Eight': " + linkedHashMap.get("Eight"));
 
             // Test containsKey
             System.out.println("\nContains 'One': " + linkedHashMap.containsKey("One"));
+            System.out.println("Contains 'Three': " + linkedHashMap.containsKey("Three"));
             System.out.println("Contains 'Four': " + linkedHashMap.containsKey("Four"));
+            System.out.println("Contains 'Eight': " + linkedHashMap.containsKey("Eight"));
+
+            // Test removal
+            System.out.println("\nRemove 'Two': " + linkedHashMap.remove("Two"));
+            System.out.println("Remove 'Ten': " + linkedHashMap.remove("Ten"));
+
+            // Print current state of the LinkedHashMap
+            System.out.println("\nCurrent Map (insertion order): " + linkedHashMap.toString());
+            
 
         } catch (Exception e) {
             System.out.println("An error occurred: " + e.getMessage());
