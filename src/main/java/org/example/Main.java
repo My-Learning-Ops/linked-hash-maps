0cb2e7f02544317123e49b1368d22132ca717b44
Brendan Dileo <dileob23@gmail.com>
2025-07-18 09:32:09 -0400

    Add containsKey test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,13 +10,17 @@ public class Main {
             linkedHashMap.put("One", 1);
             linkedHashMap.put("Two", 2);
             linkedHashMap.put("Three", 3);
-            
+
             // Update existing keys value
             linkedHashMap.put("One", 11);
 
             // Test retrieval
             System.out.println("\nGet 'Two': " + linkedHashMap.get("Two"));
             System.out.println("Get 'Four': " + linkedHashMap.get("Four"));
+
+            // Test containsKey
+            System.out.println("\nContains 'One': " + linkedHashMap.containsKey("One"));
+            System.out.println("Contains 'Four': " + linkedHashMap.containsKey("Four"));
 
         } catch (Exception e) {
             System.out.println("An error occurred: " + e.getMessage());
